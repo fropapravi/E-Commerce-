8c691a315c35196040b89ce8cd84a5c37e8ba202
Praveen Kumar B <157269668+fropapravi@users.noreply.github.com>
2024-07-28 12:37:33 +0530

    Update SQL Query Problem-2.sql

--- SQL Query Problem-2.sql
+++ SQL Query Problem-2.sql
@@ -11,17 +11,7 @@
     WHERE S.Saledate BETWEEN '2023-03-11' AND '2023-03-17' 
     GROUP BY S.Saledate 
   
-#2. Which product had the highest sales volume during the sale? 
-    SELECT SUM(P.Originalprice*S.Quantitysold) as revenue, 
-    S.Saledate 
-    FROM Products P 
-    LEFT JOIN  
-    Sales S 
-    ON P.Productid = S.Productid 
-    WHERE S.Saledate BETWEEN '2023-03-11' AND '2023-03-17' 
-    GROUP BY S.Saledate 
-  
-#3. What was the total discount given during the sale period? 
+#2. What was the total discount given during the sale period? 
   
     SELECT  
     ROUND(SUM(P.Originalprice * S.Quantitysold * P.Discountrate / 100)) AS 
@@ -33,7 +23,7 @@
     WHERE S.Saledate BETWEEN '2023-03-11' AND '2023-03-17' 
     GROUP BY  P.Productname 
   
-#4.  How does the sale performance compare in terms of units sold before and during the sale? 
+#3.  How does the sale performance compare in terms of units sold before and during the sale? 
   
     SELECT 
     SUM(CASE WHEN S.Saledate BETWEEN '2023-03-11' AND '2023-03-17' THEN 
@@ -42,7 +32,7 @@
     S.Quantitysold ELSE 0 END) AS units_sold_before_sale 
     FROM Sales S 
   
-#5. What was the average discount rate applied to products sold during the sale? 
+#4. What was the average discount rate applied to products sold during the sale? 
   
     SELECT ROUND(AVG(P.Discountrate),1) AS average_discount 
     FROM Products P 
@@ -50,7 +40,7 @@
     ON P.Productid = S.Productid 
     WHERE S.Saledate BETWEEN '2022-03-11' AND '2023-03-17' 
   
-#6. Which day had the highest revenue, and what was the top-selling product on that day? 
+#5. Which day had the highest revenue, and what was the top-selling product on that day? 
   
     SELECT ROUND(SUM(P.OriginalPrice * S.QuantitySold * (1 - P.DiscountRate / 100))) 
     AS Revenue, 
@@ -62,7 +52,7 @@
     GROUP BY S.Saledate, P.Productname 
     ORDER BY Revenue DESC 
   
-#7. How many units were sold per product category during the sale?  
+#6. How many units were sold per product category during the sale?  
   
     SELECT CASE  
       WHEN P.Productid =  1 then 'Laptop' 
@@ -78,7 +68,7 @@
     WHERE S.Saledate BETWEEN '2023-03-11' AND '2023-03-17' 
     GROUP BY Category 
   
-#8. What was the total number of transactions each day? 
+#7. What was the total number of transactions each day? 
   
     SELECT S.Saledate, COUNT(S.Quantitysold), SUM(P.Originalprice) as Revenue 
     FROM Products P 
@@ -88,7 +78,7 @@
     GROUP BY S.Saledate 
     ORDER BY S.Saledate ASC 
   
-#9. Which product had the largest discount impact on revenue? 
+#8. Which product had the largest discount impact on revenue? 
   
     SELECT P.Productname, ROUND(SUM(P.OriginalPrice * S.QuantitySold * 
     P.DiscountRate / 100)) AS Discount_Impact 
